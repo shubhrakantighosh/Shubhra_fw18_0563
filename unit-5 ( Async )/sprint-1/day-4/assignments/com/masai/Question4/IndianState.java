33a338f12a4d9de13e4bf2aa5c68ce9e68383985
shubhrakantighosh <shubhrakantighosh1996@gmail.com>
2022-09-04 01:01:17 +0530

    update assignment day 3

--- unit-5 ( Async )/sprint-1/day-4/assignments/com/masai/Question4/IndianState.java
+++ unit-5 ( Async )/sprint-1/day-4/assignments/com/masai/Question4/IndianState.java
@@ -1,26 +1,38 @@
 package com.masai.Question4;
 
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class IndianState {
 	public static void main(String[] args) {
 		
-		Map<String,String>map=new TreeMap<>(Collections.reverseOrder());
-		map.put("West Bengal","Kolkata");
-		map.put("Delhi","Delhi");
-		map.put("Telangana","Hydrabad");
-		map.put("Tamil Nadu","Chennai");
-		map.put("Punjab","Chandigar");
+		List<String>kolkata=Arrays.asList("Bardhaman","Katwa","Durgapur","Barasat");
+		List<String>delhi=Arrays.asList("Siri","New Delhi","Firozabad","Mehrauli");
+		List<String>telangana=Arrays.asList("Warangal","Hyderabad","Nizamabad","Khammam");
+		List<String>tamilNadu=Arrays.asList("Chennai","Coimbatore","Tiruchirapalli","Erode");
+		List<String>punjab=Arrays.asList("Chandigar","Ludhiana","Amritsar","Jalandhar");
 		
-		Set<Entry<String,String>> set=map.entrySet();
+		Map<String,List>map=new HashMap<>();
+		map.put("West Bengal",kolkata);
+		map.put("Delhi",delhi);
+		map.put("Telangana",telangana);
+		map.put("Tamil Nadu",tamilNadu);
+		map.put("Punjab",punjab);
 		
-		for(Entry<String,String>sets:set) {
-			System.out.println(sets.getKey()+" "+sets.getValue());
+		Set<String>states=map.keySet();
+		List<String>listOfstate=new ArrayList<>(states);
+		Collections.sort(listOfstate);
+		Collections.reverse(listOfstate);
+		
+		for(String state:listOfstate) {
+			System.out.println(state+" "+map.get(state.toString()));
 		}
+		
 		
 	}
 }
