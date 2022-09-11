148c5e9af168313b6551ca779a64bdbe6671df4d
shubhrakantighosh <shubhrakantighosh1996@gmail.com>
2022-09-11 21:16:09 +0530

    update assignment day 7

--- unit-5 ( Async )/sprint-2/day-4/assignments/com/masai/Question3/Main.java
+++ unit-5 ( Async )/sprint-2/day-4/assignments/com/masai/Question3/Main.java
@@ -3,15 +3,26 @@ package com.masai.Question3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		List<Integer>list=Arrays.asList(1,2,3,4,5,6,7);
-		
-		List<Integer>anotherList=
 
-		anotherList.forEach(System.out::println);
+		List<Integer>squareList=list
+				                    .stream()
+				                    .map(s->(s*s))
+				                    .collect(Collectors.toList());
+		
+		for(Integer integer:list) {
+			System.out.println(integer);
+		}
+		for(Integer square :squareList) {
+			System.out.println(square);
+		}
+		
+		
 	}
 
 }
--- unit-5 ( Async )/sprint-2/day-4/assignments/com/masai/Question4/Main.java
+++ unit-5 ( Async )/sprint-2/day-4/assignments/com/masai/Question4/Main.java
@@ -1,7 +1,15 @@
 package com.masai.Question4;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.masai.Question2.Student;
 
@@ -11,23 +19,42 @@ public class Main {
 		List<Student>list=Arrays.asList(
 				
 				new Student(123, "Shubhra", "skghosh@gmail.com", 470),
-				new Student(580, "Chirag", "c@gmail.com", 310),
+				new Student(580, "Chirag", "c@gmail.com", 412),
 				new Student(751, "Alex", "alex@gmail.com", 570),
-				new Student(951, "Putin", "putin@gmail.com", 210),
+				new Student(951, "Putin", "putin@gmail.com", 610),
 				new Student(841, "Robert", "robert@gmail.com", 210),
 				new Student(611, "Steave", "stave@gmail.com", 410),
+				new Student(151, "Tim", "tim@gmail.com", 310),
 				new Student(151, "Tim", "tim@gmail.com", 310)
 				
 				);
 		
-		int c=0;
 		
-		long r=list
-				
+		Comparator<Student>comparator=(s1,s2)->{
+			return s1.getMarks()>s2.getMarks() ? +1:-1;
+		};
+		
+		Set<Student>set=new TreeSet<>(comparator);
+		
+		list		
 		.stream()
-		.filter(s->s.getMarks()<500).count();
+		.filter(s->s.getMarks()<500)
+		.forEach(d->set.add(d));
 		
-		System.out.println(r);
+		for(Student student:set) {
+			System.out.println(student);
+		}
 		
+		System.out.println("\n"+"Another Way"+"\n");
+		
+		List<Student>filteredList=list
+				                     .stream()
+				                     .filter(mark->mark.getMarks()<500)
+				                     .collect(Collectors.toList());
+		
+		
+		for(Student students:filteredList) {
+			System.out.println(students);
+		}
 	}
 }
